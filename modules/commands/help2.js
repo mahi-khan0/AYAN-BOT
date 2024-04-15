8d0f5c85c919a4767faa754f68a9f19dbba5545d
mahi-khan0 <161739658+mahi-khan0@users.noreply.github.com>
2024-04-15 09:27:31 +0600

    Update help2.js

--- modules/commands/help2.js
+++ modules/commands/help2.js
@@ -1,210 +1,59 @@
+const fs = require('fs');
+const path = require('path');
+
 module.exports.config = {
- name: "help2",
- version: "1.0.2",
- hasPermission: 0,
- credits: "Mirai Team & Mod by Yan Maglinte",
- description: "Beginner's Guide",
- usePrefix: true,
- commandCategory: "guide",
- usages: "[Shows Commands]",
- cooldowns: 0,
- envConfig: {
-	autoUnsend: false,
-	delayUnsend: 60
- }
+	name: "help2",
+	version: "1.0.0",
+	hasPermission: 0,
+	credits: "AYAN",//dont change credits
+	usePrefix: true,
+	description: "all available commands by category.",
+	commandCategory: "GUIDE",
+	cooldowns: 5,
+	envConfig: {
+		autoUnsend: false,
+		delayUnsend: 2000
+	}
 };
 
-module.exports.languages = {
- en: {
-	moduleInfo:
-	 "「 %1 」\n%2\n\n❯ Usage: %3\n❯ Category: %4\n❯ Waiting time: %5 seconds(s)\n❯ Permission: %6\n\n» Module code by %7 ",
-	helpList:
-	 `₪ There are %1 commands and %2 categories on this bot.`,
-	guideList:
-	 `₪ Use: "%1${this.config.name} ‹command›" to know how to use that command!\n₪ Type: "%1${this.config.name} ‹page_number›" to show that page contents!`,
-	user: "User",
-	adminGroup: "Admin group",
-	adminBot: "Admin bot",
- },
-};
+module.exports.run = async ({ api, event }) => {
+	const commandsPath = path.join(__dirname, '..', 'commands');
 
+	try {
+		const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
-module.exports.handleEvent = function({ api, event, getText }) {
- const { commands } = global.client;
- const { threadID, messageID, body } = event;
+		if (commandFiles.length === 0) {
+			api.sendMessage("There are no commands available.", event.threadID);
+			return;
+		}
 
- if (!body || typeof body == "undefined" || body.indexOf("help") != 0)
-	return;
- const splitBody = body.slice(body.indexOf("help")).trim().split(/\s+/);
- if (splitBody.length == 1 || !commands.has(splitBody[1].toLowerCase())) return;
- const threadSetting = global.data.threadData.get(parseInt(threadID)) || {};
- const command = commands.get(splitBody[1].toLowerCase());
- const prefix = threadSetting.hasOwnProperty("PREFIX")
-	? threadSetting.PREFIX
-	: global.config.PREFIX;
- return api.sendMessage(
-	getText(
-	 "moduleInfo",
-	 command.config.name,
-	 command.config.description,
-	 `${prefix}${command.config.name} ${command.config.usages ? command.config.usages : ""
-	 }`,
-	 command.config.commandCategory,
-	 command.config.cooldowns,
-	 command.config.hasPermission === 0
-		? getText("user")
-		: command.config.hasPermission === 1
-		 ? getText("adminGroup")
-		 : getText("adminBot"),
-	 command.config.credits
-	),
-	threadID,
-	messageID
- );
-};
+		const categories = {};
+		let totalCommandList = 0; // Added to keep track of the total number of commands
 
-module.exports.run = async function({ api, event, args, getText }) {
- const { commands } = global.client;
- const { threadID, messageID } = event;
- const command = commands.get((args[0] || "").toLowerCase());
- const threadSetting = global.data.threadData.get(parseInt(threadID)) || {};
- const { autoUnsend, delayUnsend } = global.configModule[this.config.name];
- const prefix = threadSetting.hasOwnProperty("PREFIX")
-	? threadSetting.PREFIX
-	: global.config.PREFIX;
+		commandFiles.forEach(file => {
+			const commandName = path.basename(file, '.js');
+			const filePath = path.join(commandsPath, file);
+			const commandModule = require(filePath);
+			const commandConfig = commandModule.config;
 
- if (!command) {
-	const commandList = Array.from(commands.values());
-	const categories = new Set(commandList.map((cmd) => cmd.config.commandCategory.toLowerCase()));
-	const categoryCount = categories.size;
+			if (!categories[commandConfig.commandCategory]) {
+				categories[commandConfig.commandCategory] = [];
+			}
 
-	const categoryNames = Array.from(categories);
-	const itemsPerPage = 10;
-	const totalPages = Math.ceil(categoryNames.length / itemsPerPage);
+			categories[commandConfig.commandCategory].push(commandConfig.name);
+			totalCommandList++; // Increment totalCommandList for each command
+		});
 
-	let currentPage = 1;
-	if (args[0]) {
-	 const parsedPage = parseInt(args[0]);
-	 if (
-		!isNaN(parsedPage) &&
-		parsedPage >= 1 &&
-		parsedPage <= totalPages
-	 ) {
-		currentPage = parsedPage;
-	 } else {
-		return api.sendMessage(
-		 `⌘Oops! You went too far! Please choose a page between 1 and ${totalPages}⌘`,
-		 threadID,
-		 messageID
-		);
-	 }
+		let message = '';
+		for (const category in categories) {
+			const commandList = categories[category].map(command => `.${command}`).join(' │');
+			message += `╭────────────❍\n│  ${category}\n├───✦\n│${commandList}\n├───✦\n╰───────────⧕\n\n`;
+		}
+
+		message += `╭────────────❍\n│ » ${global.config.BOTNAME} has ${totalCommandList} cmds\n│ » Type .help <cmd>\n│ to show the command information\n╰────────────❍`;
+		api.sendMessage(message, event.threadID);    
+	} catch (error) {
+		console.error('Error listing commands:', error);
+		api.sendMessage("An error occurred while listing commands.", event.threadID);
 	}
-
-	const startIdx = (currentPage - 1) * itemsPerPage;
-	const endIdx = startIdx + itemsPerPage;
-	const visibleCategories = categoryNames.slice(startIdx, endIdx);
-
-	let msg = "";
-	for (let i = 0; i < visibleCategories.length; i++) {
-	 const category = visibleCategories[i];
-	 const categoryCommands = commandList.filter(
-		(cmd) =>
-		 cmd.config.commandCategory.toLowerCase() === category
-	 );
-
-	 const commandNames = categoryCommands.map((cmd) => cmd.config.name);
-	 const numberFont = [
-		"❶",
-		"❷",
-		"❸",
-		"❹",
-		"❺",
-		"❻",
-		"❼",
-		"❽",
-		"❾",
-		"❿",
-	 ];
-	 msg += `╭[ ${numberFont[i]} ]─❍ ${category.charAt(0).toUpperCase() + category.slice(1)
-		}\n╰┈➤  》${commandNames.join(", ")}\n\n`;
-	}
-
-	const numberFontPage = [
-	 "❶",
-	 "❷",
-	 "❸",
-	 "❹",
-	 "❺",
-	 "❻",
-	 "❼",
-	 "❽",
-	 "❾",
-	 "❿",
-	 "⓫",
-	 "⓬",
-	 "⓭",
-	 "⓮",
-	 "⓯",
-	 "⓰",
-	 "⓱",
-	 "⓲",
-	 "⓳",
-	 "⓴",
-	];
-	msg += `╭ ───────── ╮
-│ 𝖯𝖠𝖦𝖤 ${numberFontPage[currentPage - 1]} of ${numberFontPage[totalPages - 1]
-	 }  │\n╰ ───────── ╯\n`;
-	msg += getText("helpList", commands.size, categoryCount, prefix);
-
-	const axios = require("axios");
-	const fs = require("fs-extra");
-	const imgP = [];
-	const img = [
-"https://i.imgur.com/ipOvlWn.gif",
-"https://i.imgur.com/7ff1e8v.gif",
-"https://i.imgur.com/hvcUCC5.gif",
-"https://i.imgur.com/fKKULWx.gif",
-	];
-	const path = __dirname + "/cache/menu.png";
-	const rdimg = img[Math.floor(Math.random() * img.length)];
-
-	const { data } = await axios.get(rdimg, {
-	 responseType: "arraybuffer",
-	});
-
-	fs.writeFileSync(path, Buffer.from(data, "utf-8"));
-	imgP.push(fs.createReadStream(path));
-	const config = require("./../../config.json")
-	const msgg = {
-	 body: `╭━─━─━──━─━≪✠≫━─━─━─━─━╮\n |━❮  𝖢𝖮𝖬𝖬𝖠𝖭𝖣𝖲  ●  𝖢𝖠𝖳𝖤𝖦𝖮𝖱𝖸 ❯━|╰━─━─━━─━─━≪✠≫━─━─━─━─━╯\n𒁍 𝖡𝗈𝗍 𝖮𝗐𝗇𝖾𝗋: ${config.DESIGN.Admin}\n\n` + msg + `\n❒Total pages available: ${totalPages}.\n` + `\n╭ ───── ╮\n│  𝖦𝗎𝗂𝖽𝖾   │\n╰ ───── ╯\n` + getText("guideList", config.PREFIX),
-	 attachment: imgP,
-	};
-
-	const sentMessage = await api.sendMessage(msgg, threadID, messageID);
-
-	if (autoUnsend) {
-	 setTimeout(async () => {
-		await api.unsendMessage(sentMessage.messageID);
-	 }, delayUnsend * 1000);
-	}
- } else {
-	return api.sendMessage(
-	 getText(
-		"moduleInfo",
-		command.config.name,
-		command.config.description,
-		`${prefix}${command.config.name} ${command.config.usages ? command.config.usages : ""
-		}`,
-		command.config.commandCategory,
-		command.config.cooldowns,
-		command.config.hasPermission === 0
-		 ? getText("user")
-		 : command.config.hasPermission === 1
-			? getText("adminGroup")
-			: getText("adminBot"),
-		command.config.credits
-	 ),
-	 threadID, messageID
-	);
- }
 };
